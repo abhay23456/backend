12f7994a5c600488f09bf0e656970aa1bf828f06
abhayw1311 <98542901+abhayw1311@users.noreply.github.com>
2024-03-30 17:09:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express()
+// const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('Abhay Wankhade')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
